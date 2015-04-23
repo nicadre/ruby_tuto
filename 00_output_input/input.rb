09786a2e66bb12b4e10cb0e362c3488608ccae01
Nicolas Chevalier <niccheva@student.42.fr>
2015-04-23 10:24:19 +0200

    output input

---
+++ 00_output_input/input.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+puts "Enter A"
+a = gets.chomp
+#a = STDIN.gets.chomp
+puts "Enter B"
+b = gets.chomp
+#b = STDIN.gets.chomp
+c = a.to_i + b.to_i
+puts c
---
+++ 00_output_input/output.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+puts "a" + "b"
+puts "abc" * 3
+puts 24.to_s.reverse
+
+name = "seb"
+puts name.capitalize
+puts name.length
