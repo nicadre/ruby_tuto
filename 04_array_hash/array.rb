b6be75436b7ccbe8c8a8e06580b6b8f4c18f80db
Nicolas Chevalier <niccheva@student.42.fr>
2015-04-24 10:39:15 +0200

    array hash:

---
+++ 04_array_hash/array.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+names = ["toto", "tutu", "tata"]
+
+names.each do |name|
+  puts name
+end
+
+puts "names are empty? #{names.nil?}"
---
+++ 04_array_hash/hash.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
+
+puts grades["Jim Doe"]
+
+options = { :font_size => 10, :font_family => "Arial" }
+options = { font_size: 10, font_family: "Arial" }
+
+puts options[:font_family]
+
+
